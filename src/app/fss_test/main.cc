6712db051a9d48ca64d9288a7a3a540c9539bb6e
hhami.2040 <hhami.2040@gmail.com>
2026-07-19 05:45:03 -0400

    feat: setup stable base for fss_test component

--- src/app/fss_test/main.cc
+++ src/app/fss_test/main.cc
@@ -1,20 +1,8 @@
 #include <base/component.h>
 #include <base/log.h>
-#include <base/heap.h>
-#include <base/allocator_avl.h> //requited
-#include <file_system_session/connection.h>
 
-void Component::construct(Genode::Env &env) {
-    Genode::log("--- [fss_test] Initializing via Official Pattern ---");
-
-    // ۱. ساخت Heap برای مدیریت کلی حافظه
-    static Genode::Heap heap(env.ram(), env.rm());
-
-    // ۲. ساخت Allocator اختصاصی برای سشن فایل‌سیستم
-    static Genode::Allocator_avl fs_tx_block_alloc(&heap);
-
-    // ۳. ایجاد کانکشن (فقط با پاس دادن allocator)
-    File_system::Connection fs(env, fs_tx_block_alloc);
-
-    Genode::log("--- [fss_test] Connection Created Successfully! ---");
+void Component::construct(Genode::Env &env)
+{
+    (void)env; 
+    Genode::log("--- [fss_test] Component started successfully! ---");
 }
--- src/app/fss_test/target.mk
+++ src/app/fss_test/target.mk
@@ -1,11 +1,3 @@
 TARGET = fss_test
 SRC_CC = main.cc
-
-
-LIBS = base
-
-
-INC_DIR += $(GENODE_DIR)/repos/os/include
-
-
-# LDFLAGS += -los
+LIBS   = base
